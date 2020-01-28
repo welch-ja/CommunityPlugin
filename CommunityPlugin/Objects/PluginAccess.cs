59588f93384c853c4411e2943457a79bd545d5bd
Zach Sharkey <zach.sharkey@amecinc.com>
2020-01-28 14:23:37 -0600

    Fixing Pluginaccess and adding descriptor check to virtual fields.

--- CommunityPlugin/Objects/PluginAccess.cs
+++ CommunityPlugin/Objects/PluginAccess.cs
@@ -15,9 +15,6 @@ namespace CommunityPlugin.Objects
 
         public static bool CheckAccess(string pluginName, bool menu = false, bool loan = false)
         {
-            if (!EncompassHelper.User.ID.Equals("zsharkey") && !EncompassHelper.User.ID.Equals("rulelock"))
-                return false;
-
             if (EncompassHelper.IsTest() || CDOHelper.CDO.CommunitySettings.SuperAdminRun)
                 return true;
 
